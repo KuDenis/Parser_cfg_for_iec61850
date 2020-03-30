5776ec387c8a2e877fce856465bfbc1d8427c645
KuDenis <62889734+KuDenis@users.noreply.github.com>
2020-03-30 22:44:16 +0700

    added cfg file and easy test

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,6 @@
 
 def sum(a, b):
     return a + b
+
+def mull(a, b):
+    return a * b
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -2,7 +2,15 @@ from src import parser
 
 
 def test_sum():
+    a = [*range(9)]
+    b = [*range(9)]
+    result = [2*x for x in range(9)]
+    for c, d, e in zip(a, b, result):
+        assert parser.sum(c, d) == e
+
+
+def test_mull():
     a = 2
     b = 3
-    result = 5
-    assert parser.sum(a, b) == result
+    result = 6
+    assert parser.mull(a, b) == result
